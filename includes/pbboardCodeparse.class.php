3a5cc654d9177a182c8358a6ab53efbed7435644
PBBoard Forum Software <soliman0dawood@gmail.com>
2023-03-29 03:13:33 +0300

    Update pbboardCodeparse.class.php

--- includes/pbboardCodeparse.class.php
+++ includes/pbboardCodeparse.class.php
@@ -234,7 +234,7 @@ class PowerBBCodeParse
 			$string = preg_replace_callback($regexcode_code, function($matches) {
 			$matches[1] = base64_decode($matches[1]);
 			//$matches[1] = str_replace('<br />', '&nbsp;', $matches[1]);
-			//$matches[1] = $this->htmlspecialchars_uni($matches[1]);
+			$matches[1] = $this->htmlspecialchars_uni($matches[1]);
 			$matches[1] = str_replace('&amp;', '&amp;amp;', $matches[1]);
 			$matches[1] = str_replace('&amp;nbsp;', '&nbsp;', $matches[1]);
 			return '<div class="maxy"></div><div class="codediv">CODE</div><pre><code class="language-php">'.$matches[1].'</code></pre><div class="maxy"></div>';
